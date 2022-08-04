4c7eeb5b7f37b39197c1239fcf8598d031d13121
Courtney Van Breda <cmvanbreda91@gmail.com>
2022-08-04 14:57:07 -0400

    setting and getting items from local storage working

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,6 +34,7 @@ var four = {hour:16, minute:00}
 $("#newPId7").text("4:00pm")
 var five = {hour:17, minute:00}
 $("#newPId8").text("5:00pm")
+var six  = {hour:18, minute:00}
 
 
 
@@ -132,7 +133,28 @@ else if (dateToday>=Date.today().at(four) && dateToday<Date.today().at(five)){
         $("#newRowId7").addClass("present")
 }
 
- else if (dateToday>Date.today().at(five)) {
+else if (dateToday>=Date.today().at(five) && dateToday<Date.today().at(six)){
+        $("#newRowId0").removeClass("future")
+        $("#newRowId0").addClass("past")
+        $("#newRowId1").removeClass("future")
+        $("#newRowId1").addClass("past")
+        $("#newRowId2").removeClass("future")
+        $("#newRowId2").addClass("past")
+        $("#newRowId3").removeClass("future")
+        $("#newRowId3").addClass("past")
+        $("#newRowId4").removeClass("future")
+        $("#newRowId4").addClass("past")
+        $("#newRowId5").removeClass("future")
+        $("#newRowId5").addClass("past")
+        $("#newRowId6").removeClass("future")
+        $("#newRowId6").addClass("past")
+        $("#newRowId7").removeClass("future")
+        $("#newRowId7").addClass("past")
+        $("#newRowId8").removeClass("future")
+        $("#newRowId8").addClass("present")
+}
+
+ else if (dateToday>Date.today().at(six)) {
         $("#newRowId0").removeClass("future")
         $("#newRowId0").addClass("past")
         $("#newRowId1").removeClass("future")
@@ -153,19 +175,94 @@ else if (dateToday>=Date.today().at(four) && dateToday<Date.today().at(five)){
         $("#newRowId8").addClass("past")
 }
 
+else if (dateToday===dayStart) {
+        localStorage.clear()
+}
 
-var textNine = $("#newTextId0").val();
-var textTen = "";
-var textEleven = "";
-var textTwelve = "";
-var textOne = "";
-var textTwo = "";
-var textThree = "";
-var textFour = "";
-var textFive =  "";
-var taskAssignment ="";
+var toDoInput0 = "";
+var toDoInput1 = "";
+var toDoInput2 = "";
+var toDoInput3 = "";
+var toDoInput4 = "";
+var toDoInput5 = "";
+var toDoInput6 = "";
+var toDoInput7 = "";
+var toDoInput8 = "";
 
 
+$("#newBtnId0").on("click", function (event){
+        event.preventDefault();
+        toDoInput0 = $("#newTextId0").val();
+        localStorage.setItem("toDoInput0", JSON.stringify(toDoInput0));
+})
 
-$("#newBtnId0").click(localStorage.setItem("taskAssignment0", JSON.stringify(textNine)))
-        
+$("#newBtnId1").on("click", function (event){
+        event.preventDefault();
+        toDoInput1 = $("#newTextId1").val();
+        localStorage.setItem("toDoInput1", JSON.stringify(toDoInput1));
+})
+
+$("#newBtnId2").on("click", function (event){
+        event.preventDefault();
+        toDoInput2 = $("#newTextId2").val();
+        localStorage.setItem("toDoInput2", JSON.stringify(toDoInput2));
+})
+
+$("#newBtnId3").on("click", function (event){
+        event.preventDefault();
+        toDoInput3 = $("#newTextId3").val();
+        localStorage.setItem("toDoInput3", JSON.stringify(toDoInput3));
+})
+
+$("#newBtnId4").on("click", function (event){
+        event.preventDefault();
+        toDoInput4 = $("#newTextId4").val();
+        localStorage.setItem("toDoInput4", JSON.stringify(toDoInput4));
+})
+
+$("#newBtnId5").on("click", function (event){
+        event.preventDefault();
+        toDoInput5 = $("#newTextId5").val();
+        localStorage.setItem("toDoInput5", JSON.stringify(toDoInput5));
+})
+
+$("#newBtnId6").on("click", function (event){
+        event.preventDefault();
+        toDoInput6 = $("#newTextId6").val();
+        localStorage.setItem("toDoInput6", JSON.stringify(toDoInput6));
+})
+
+$("#newBtnId7").on("click", function (event){
+        event.preventDefault();
+        toDoInput7 = $("#newTextId7").val();
+        localStorage.setItem("toDoInput7", JSON.stringify(toDoInput7));
+})
+
+$("#newBtnId8").on("click", function (event){
+        event.preventDefault();
+        toDoInput8 = $("#newTextId8").val();
+        localStorage.setItem("toDoInput8", JSON.stringify(toDoInput8));
+})
+
+function getLocalStorage(){
+        toDoInput0 = JSON.parse(localStorage.getItem('toDoInput0'));
+        toDoInput1 = JSON.parse(localStorage.getItem('toDoInput1'));
+        toDoInput2 = JSON.parse(localStorage.getItem('toDoInput2'));
+        toDoInput3 = JSON.parse(localStorage.getItem('toDoInput3'));
+        toDoInput4 = JSON.parse(localStorage.getItem('toDoInput4'));
+        toDoInput5 = JSON.parse(localStorage.getItem('toDoInput5'));
+        toDoInput6 = JSON.parse(localStorage.getItem('toDoInput6'));
+        toDoInput7 = JSON.parse(localStorage.getItem('toDoInput7'));
+        toDoInput8 = JSON.parse(localStorage.getItem('toDoInput8'));
+        $("#newTextId0").val(toDoInput0);
+        $("#newTextId1").val(toDoInput1);
+        $("#newTextId2").val(toDoInput2);
+        $("#newTextId3").val(toDoInput3);
+        $("#newTextId4").val(toDoInput4);
+        $("#newTextId5").val(toDoInput5);
+        $("#newTextId6").val(toDoInput6);
+        $("#newTextId7").val(toDoInput7);
+        $("#newTextId8").val(toDoInput8);
+}
+
+getLocalStorage();
